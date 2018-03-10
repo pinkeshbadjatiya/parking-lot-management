51247b9593dc8d3a5998a10642797657d5b7439e
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2018-03-10 20:54:06 +0530

    Update server config

--- server_config.py
+++ server_config.py
@@ -2,6 +2,6 @@ DEBUG = True
 # TESTING = True
 JWT_SECRET = 'some-secret-string'
 SECRET_KEY = 'fskfjskgflaj'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://plclientuser:client@pl@10.2.5.98/' \
-                          'plclientdb'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://plserveruser:server@pl@10.2.5.98/' \
+                          'plserverdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
