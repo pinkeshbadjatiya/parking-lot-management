faf46c897b015f4830f2550e6a5d47903edca169
Neel <neel@localhost.localdomain>
2018-03-26 12:08:05 +0530

    creating tables and snapshot

---
+++ Misc/snapshot.py
@@ -0,0 +1,34 @@
+import random as rand
+
+def print_snap(snap):
+    for item in snap:
+        print len(item), item
+        print
+
+def req_print(snap):
+    s = ""
+    for i in range(len(snap)):
+        curr = snap[i]
+        for j in range(len(curr)):
+            s += str(curr[j])
+            if(j != len(curr) - 1):
+                s += ','
+        if(i != len(snap) - 1):
+            s += '#'
+
+    print s
+            
+
+def make_snap():
+    snap = []
+    part = []
+    for i in  range(0,7):
+        part = []
+        for j in range(0, 24):
+            part.append(rand.uniform(100,500))
+
+        snap.append(part)
+
+    req_print(snap)
+
+make_snap()
