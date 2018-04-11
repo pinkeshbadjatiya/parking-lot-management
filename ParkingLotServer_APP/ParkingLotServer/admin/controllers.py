e38c527af7bfaa65e452ea62bc8c250645f70177
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2018-04-12 04:03:56 +0530

    Fix: Update Price + Beautify

--- ParkingLotServer_APP/ParkingLotServer/admin/controllers.py
+++ ParkingLotServer_APP/ParkingLotServer/admin/controllers.py
@@ -3,7 +3,7 @@ import jwt
 import datetime
 from passlib.hash import argon2
 from sqlalchemy import and_
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask import Flask, render_template, redirect, flash, Blueprint, request, jsonify, url_for
 from ParkingLotServer import app, db
 from .models import ParkingLot, Utilization, Charge
@@ -19,17 +19,25 @@ def view_update_prices():
     days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
     hoursList = ["D \ H", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
     plList = []
-    plListMap = {}
+    plListMap, plIdToNameMap = {}, {}
     plListRaw = ParkingLot.query.filter_by(pl_active='t').all()
     for plListRawItem in plListRaw:
         plList.append(plListRawItem.pl_name)
         plListMap[plListRawItem.pl_name] = plListRawItem.id
+        plIdToNameMap[plListRawItem.id] = plListRawItem.pl_name
 
     headerTitle = 'Parking Lot - Price Snapshot'
     if request.method == 'POST':
-        # Update the new received price
+
+        parking_lot_id = request.form.get("parking_lot_id")
         price_snapshot = request.form.get("price_snapshot")
-        print price_snapshot
+
+        # Reset the old active price_snapshot for that parkinglot
+        old_active_charges = Charge.query.filter(and_(Charge.pl_id == parking_lot_id, Charge.ch_active == True)).update({Charge.ch_active: False})
+        # Update the new received price
+        new_charge = Charge(parking_lot_id, price_snapshot, True)
+        db.session.add(new_charge)
+        db.session.commit()
 
         return jsonify({
             'message': 'ok'
@@ -58,6 +66,7 @@ def view_update_prices():
                                     daysList=days,
                                     hoursList=hoursList,
                                     priceDataPresent=False,
+                                    spl=plIdToNameMap[parklot_id],
                                     message="No charge data available")
 
         # Convert to lists
@@ -72,9 +81,12 @@ def view_update_prices():
                                 parkinglotname=selected_pl,
                                 daysList=days,
                                 hoursList=hoursList,
+                                parking_lot_id=parklot_id,
+                                spl=plIdToNameMap[parklot_id],
                                 pricesnapshot=price_snapshot)
 
-    return render_template('viewprices.html', headerTitle=headerTitle, parkinglotList=plList, daysList=days, hoursList=hoursList, priceDataPresent=True)
+    return render_template('viewprices.html', headerTitle=headerTitle, parkinglotList=plList, daysList=days, hoursList=hoursList,
+                            parking_lot_id=parklot_id,priceDataPresent=True)
 
 
 def get_parking_lots():
@@ -90,13 +102,11 @@ def get_parking_lots():
 @mod_admin.route('/configureParkingLots', methods=['GET', 'POST'])
 @login_required
 def configure_parking_lots():
-    # print 'Configure Parking Lots'
     plList, plListMap = get_parking_lots()
     if request.method == 'POST':
         if request.form['button'] == 'delete_pl':
             db.session.delete(plListMap[request.form['id']])
             db.session.commit()
-            print "delete request " + request.form['id']
             # delete operation to be performed
         if request.form['button'] == 'add_pl':
             parking_lot = ParkingLot(request.form['pl_name'], request.form['pl_address'], request.form['pl_capacity'], request.form['pl_price'], True)
@@ -112,7 +122,6 @@ def configure_parking_lots():
 
             # if parking_lot.id != 1 :
             #     flash('Error in adding parking lot')
-            print "add request"
     plList, plListMap = get_parking_lots()
     return render_template('parkinglots.html', headerTitle='Parking Lot - Configuration', parkinglotList=plList)
 
@@ -181,4 +190,4 @@ def view_utilization():
 @mod_admin.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def show_dashboard():
-    return render_template('dashboard.html', headerTitle='Parking Lot - Dashboard')
+    return render_template('dashboard.html', headerTitle='Parking Lot - Dashboard', username=current_user.first_name + " " + current_user.last_name)
